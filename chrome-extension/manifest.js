bf8bdeb15de793a321a6d26596d2ea8d52fd0355
Willie Pai <Willie.Pai@soti.net>
2024-08-19 16:09:57 -0400

    feat: remove popup, options, etc.

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -6,9 +6,9 @@ const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 const isFirefox = process.env.__FIREFOX__ === 'true';
 
 const sidePanelConfig = {
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
+  // side_panel: {
+  //   default_path: 'side-panel/index.html',
+  // },
   permissions: ['sidePanel'],
 };
 
@@ -29,18 +29,18 @@ const manifest = deepmerge(
     description: '__MSG_extensionDescription__',
     host_permissions: ['<all_urls>'],
     permissions: ['storage', 'scripting'],
-    options_page: 'options/index.html',
-    background: {
-      service_worker: 'background.iife.js',
-      type: 'module',
-    },
-    action: {
-      default_popup: 'popup/index.html',
-      default_icon: 'icon-34.png',
-    },
-    chrome_url_overrides: {
-      newtab: 'new-tab/index.html',
-    },
+    // options_page: 'options/index.html',
+    // background: {
+    //   service_worker: 'background.iife.js',
+    //   type: 'module',
+    // },
+    // action: {
+    //   default_popup: 'popup/index.html',
+    //   default_icon: 'icon-34.png',
+    // },
+    // chrome_url_overrides: {
+    //   newtab: 'new-tab/index.html',
+    // },
     icons: {
       128: 'icon-128.png',
     },
@@ -58,7 +58,7 @@ const manifest = deepmerge(
         css: ['content.css'], // public folder
       },
     ],
-    devtools_page: 'devtools/index.html',
+    // devtools_page: 'devtools/index.html',
     web_accessible_resources: [
       {
         resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
